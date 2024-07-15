7a3e81c9602206eb86064482dad3cf12311802be
Reza <rez@mail.com>
2024-07-15 19:48:49 +0330

    override error attribute

---
+++ src/main/java/com/snapp/dangidongi/configuration/DangiDongiErrorAttribute.java
@@ -0,0 +1,34 @@
+package com.snapp.dangidongi.configuration;
+
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class DangiDongiErrorAttribute extends DefaultErrorAttributes {
+
+    @Override
+    public Map<String, Object> getErrorAttributes(
+            WebRequest webRequest, ErrorAttributeOptions options) {
+        Map<String, Object> errorAttributes =
+                super.getErrorAttributes(webRequest, options);
+        errorAttributes.put("errorCode", "0000");
+        errorAttributes.put("description", errorAttributes.get("error"));
+        HashMap<String, Object> metadata = new HashMap<>();
+        metadata.put("path", errorAttributes.get("path"));
+        metadata.put("trace", errorAttributes.get("trace"));
+        metadata.put("exception", errorAttributes.get("exception"));
+        errorAttributes.put("metadata", metadata);
+        errorAttributes.remove("error");
+        errorAttributes.remove("errors");
+        errorAttributes.remove("timestamp");
+        errorAttributes.remove("status");
+        errorAttributes.remove("path");
+        errorAttributes.remove("exception");
+        return errorAttributes;
+    }
+}
