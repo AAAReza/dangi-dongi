acbff7193be178d176add95da9c974c5324efb27
Reza <rez@mail.com>
2024-07-15 21:13:14 +0330

    enable validation for api model add new advisor for validation message

--- src/main/java/com/snapp/dangidongi/configuration/ExceptionAdvisor.java
+++ src/main/java/com/snapp/dangidongi/configuration/ExceptionAdvisor.java
@@ -1,10 +1,15 @@
 package com.snapp.dangidongi.configuration;
 
 import com.snapp.dangidongi.common.ErrorModel;
+import com.snapp.dangidongi.common.Errors;
 import com.snapp.dangidongi.exception.DangiDongiException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class ExceptionAdvisor {
@@ -17,5 +22,14 @@ public class ExceptionAdvisor {
                 .errorCode(ex.getErrors().getCode())
                 .metadata(ex.getCause()).build();
         return new ResponseEntity<>(errorResponse, ex.getErrors().getStatus());
+    }
+
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorModel> handleConstraintViolationException(MethodArgumentNotValidException ex) {
+        ErrorModel errorModel = ErrorModel.builder().description(Errors.INVALID_REQUEST.getDescription())
+                .metadata(ex.getBindingResult().getFieldErrors().stream().map(fieldError -> "[" + fieldError.getField() + "] : " + fieldError.getDefaultMessage())
+                        .collect(Collectors.joining(", "))).errorCode(Errors.INVALID_REQUEST.getCode()).build();
+        return new ResponseEntity<ErrorModel>(errorModel, HttpStatus.BAD_REQUEST);
     }
 }
