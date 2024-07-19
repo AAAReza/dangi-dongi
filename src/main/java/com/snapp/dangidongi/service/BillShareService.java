02687b4f7591ed0f8120c672f1c6e0d2097b71cb
Reza <rez@mail.com>
2024-07-19 12:44:39 +0330

    add calculation of share in service

--- src/main/java/com/snapp/dangidongi/service/BillShareService.java
+++ src/main/java/com/snapp/dangidongi/service/BillShareService.java
@@ -1,14 +1,20 @@
 package com.snapp.dangidongi.service;
 
+import com.snapp.dangidongi.entity.BillEntity;
 import com.snapp.dangidongi.entity.BillShareEntity;
+import com.snapp.dangidongi.entity.UserEntity;
 import com.snapp.dangidongi.exception.NotFoundException;
 import com.snapp.dangidongi.mapper.BillShareMapper;
 import com.snapp.dangidongi.model.BillShareModel;
 import com.snapp.dangidongi.repository.BillShareRepository;
+import com.snapp.dangidongi.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Service
 @AllArgsConstructor
@@ -17,11 +23,34 @@ public class BillShareService {
 
     private final BillShareRepository billShareRepository;
     private final BillShareMapper billShareMapper;
+    private final UserRepository userRepository;
 
-    public BillShareEntity save(BillShareModel billShare) {
-        BillShareEntity billShareEntity = billShareMapper.billShareModelToBillShareEntity(billShare);
-        return billShareRepository.save(billShareEntity);
+
+    public void calculateBillShareInGroup(BillEntity bill, Long groupFriendId) throws NotFoundException {
+        Page<UserEntity> usersInGroup = userRepository.findByUserFriendGroups_Group_Id(groupFriendId, Pageable.unpaged());
+        BigDecimal shareOfAnyOne = bill.getAmount().divide(new BigDecimal(usersInGroup.getTotalElements()), 0, RoundingMode.FLOOR);
+        BigDecimal residueOfAmount = bill.getAmount();
+        do {
+            for (UserEntity userEntity : usersInGroup.getContent()) {
+                BillShareEntity billShareEntity = BillShareEntity.builder()
+                        .bill(bill)
+                        .userFriendGroup(userEntity.getUserFriendGroups()
+                                .stream()
+                                .filter(userFriendGroupEntity -> userFriendGroupEntity.getGroup()
+                                        .getId().equals(bill.getFriendGroup().getId()))
+                                .findFirst().get())
+                        .build();
+                if (residueOfAmount.subtract(shareOfAnyOne).compareTo(shareOfAnyOne) > 0) {
+                    billShareEntity.setShareAmount(shareOfAnyOne);
+                    residueOfAmount = residueOfAmount.subtract(shareOfAnyOne);
+                } else {
+                    billShareEntity.setShareAmount(residueOfAmount);
+                }
+                billShareRepository.save(billShareEntity);
+            }
+        } while (usersInGroup.hasNext());
     }
+
 
     public BillShareModel findById(Long id) throws NotFoundException {
         BillShareEntity billShare = billShareRepository.findById(id).orElseThrow(NotFoundException::new);
@@ -38,9 +67,17 @@ public class BillShareService {
         billShareRepository.deleteById(id);
     }
 
-    public BillShareModel getMyBillShareInGroup(Long billId, Long groupId, Long userId) throws NotFoundException {
+    public BillShareEntity getMyBillShareInGroup(Long billId, Long groupId, Long userId) throws NotFoundException {
         return billShareRepository.findByBill_IdAndUserFriendGroup_Group_IdAndUserFriendGroup_User_Id(billId, groupId, userId)
-                .map(billShareMapper::billShareEntityToBillShareModel)
                 .orElseThrow(NotFoundException::new);
+    }
+
+    public Page<BillShareEntity> getBillSharesOfBill(Long billId, Pageable pageable) {
+        return billShareRepository.findByBill_Id(billId, pageable);
+    }
+
+
+    public Page<BillShareEntity> getAllBillSharesInGroup(Long groupId, Pageable pageable) {
+        return billShareRepository.findByUserFriendGroup_Group_Id(groupId, pageable);
     }
 }
