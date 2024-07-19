ab1e6a39f58375ff869a8c7b4c822928928f3ec9
Reza <rez@mail.com>
2024-07-19 12:19:16 +0330

    add billshare entity and repository

--- src/main/java/com/snapp/dangidongi/repository/BillShareRepository.java
+++ src/main/java/com/snapp/dangidongi/repository/BillShareRepository.java
@@ -1,6 +1,8 @@
 package com.snapp.dangidongi.repository;
 
 import com.snapp.dangidongi.entity.BillShareEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,8 @@ import java.util.Optional;
 public interface BillShareRepository extends JpaRepository<BillShareEntity, Long> {
 
     Optional<BillShareEntity> findByBill_IdAndUserFriendGroup_Group_IdAndUserFriendGroup_User_Id(Long billId, Long groupId, Long userId);
+
+    Page<BillShareEntity> findByBill_Id(Long billId, Pageable pageable);
+
+    Page<BillShareEntity> findByUserFriendGroup_Group_Id(Long groupId, Pageable pageable);
 }
