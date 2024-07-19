9fac966f0f07fc8ca01671efb20c764a8eae7524
Reza <rez@mail.com>
2024-07-19 18:09:20 +0330

    fix test to run with mock user refactor and add test to postman collection

--- src/test/java/com/snapp/dangidongi/test/FriendGroupTest.java
+++ src/test/java/com/snapp/dangidongi/test/FriendGroupTest.java
@@ -7,11 +7,13 @@ import com.snapp.dangidongi.entity.Gender;
 import com.snapp.dangidongi.entity.UserEntity;
 import com.snapp.dangidongi.model.FriendGroupCreateModel;
 import com.snapp.dangidongi.repository.UserRepository;
+import com.snapp.dangidongi.security.Role;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -20,7 +22,7 @@ import java.util.Arrays;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class FriendGroupTest extends DangiDongiApplicationTests {
 
@@ -30,22 +32,24 @@ public class FriendGroupTest extends DangiDongiApplicationTests {
     private static Long userId;
     private static String groupLocation;
 
+
     @BeforeEach
     public void saveUserEntity() {
         UserEntity user = UserEntity.builder()
-                .username("aaareza" + counter++)
+                .username("dangi0" + counter++)
                 .firstname("reza")
                 .lastname("afzali")
                 .email("reza@email.com" + counter++)
                 .password("123")
                 .phone(9195264326L + counter++)
                 .gender(Gender.MALE)
+                .role(Role.ROLE_USER)
                 .birthday(LocalDate.now())
                 .build();
         userId = userRepository.save(user).getId();
     }
 
-
+    @WithMockUser(username = "dangi0")
     @Test
     public void testCreateFriendGroup() throws Exception {
         FriendGroupCreateModel friendGroupCreateModel = FriendGroupCreateModel.builder()
@@ -63,7 +67,7 @@ public class FriendGroupTest extends DangiDongiApplicationTests {
 
     }
 
-
+    @WithMockUser(username = "dangi0")
     @Test
     public void testAddUserToGroup() throws Exception {
         testGetFriendGroup();
@@ -73,7 +77,7 @@ public class FriendGroupTest extends DangiDongiApplicationTests {
 
     }
 
-
+    @WithMockUser(username = "dangi0")
     @Test
     public void testGetFriendGroup() throws Exception {
         testCreateFriendGroup();
@@ -84,6 +88,7 @@ public class FriendGroupTest extends DangiDongiApplicationTests {
 
     }
 
+    @WithMockUser(username = "dangi0")
     @Test
     public void testOwnFriendGroup() throws Exception {
         testCreateFriendGroup();
@@ -91,7 +96,7 @@ public class FriendGroupTest extends DangiDongiApplicationTests {
                         .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andReturn();
         assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus());
-        assertTrue(!objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), Map.class).isEmpty());
+        assertFalse(objectMapper.readValue(mvcResult.getResponse().getContentAsByteArray(), Map.class).isEmpty());
     }
 
 }
